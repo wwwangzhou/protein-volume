6058c2155403a44f867a9db3589ccb3904e51d4b
shihanjing <32828702+shihanjing@users.noreply.github.com>
2020-03-11 20:20:33 -0700

    Update plot.R

--- protein-volume/plot.R
+++ protein-volume/plot.R
@@ -1,4 +1,6 @@
 install.packages("tidyverse")
 library(ggplot2)
 final = read.table("~/Desktop/129.txt")
-ggplot(final, aes(x = V4, V1)) + geom_ribbon(aes(ymin = V2, ymax=V3), alpha = 0.2) +  geom_line() + labs(title = "number vs value", x = "number", y = "value")
+ggplot(final, aes(x = V4, V1)) + geom_ribbon(aes(ymin = V2, ymax=V3), alpha = 0.2) +  geom_line() + labs(title = "number vs volume", x = "N", y = "V")
+
+
