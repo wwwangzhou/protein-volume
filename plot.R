c364924348528052b86c0b537a4d7a83905393aa
shihanjing <32828702+shihanjing@users.noreply.github.com>
2020-03-11 20:07:47 -0700

    Add files via upload

---
+++ plot.R
@@ -0,0 +1,4 @@
+install.packages("tidyverse")
+library(ggplot2)
+final = read.table("~/Desktop/129.txt")
+ggplot(final, aes(x = V4, V1)) + geom_ribbon(aes(ymin = V2, ymax=V3), alpha = 0.2) +  geom_line() + labs(title = "number vs value", x = "number", y = "value")
