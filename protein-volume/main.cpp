aa024979338a1fb8bf79d6bf2569ea45bcf4512b
Z <zouwang@ucdavis.edu>
2020-03-13 16:01:57 -0700

    added sa

--- protein-volume/main.cpp
+++ protein-volume/main.cpp
@@ -8,6 +8,7 @@
 #include <limits>
 #include <iomanip>
 #include <chrono>
+#include <vector>
 
 const char input_file[] = "Volume.crd";
 const char output_file[] = "V_N.txt";
@@ -98,6 +99,9 @@ std::ostream& operator<<(std::ostream& s,const Atom& atom)
    return s;
 }
 
+/* calculate the total surface area of the union of all balls*/
+double get_total_surface_area(Atom atoms [], const unsigned& N);
+
 /* open the input file so that it is ready to be read */
 void open_file(int& size,std::ifstream& fin);
 
@@ -110,6 +114,9 @@ double get_random_double(const double& min, const double& max);
 /* get a random point within the box */
 Point get_random_point(const Box& box);
 
+/* get a random point from the surface of all balls */
+Point get_random_point_prime(const Atom& atom);
+
 /* get the distance between two points */
 double get_distance(const Point& p, const Atom& a);
 
@@ -117,10 +124,19 @@ double get_distance(const Point& p, const Atom& a);
    is lesser than its radius */
 int calculate_fx(const Point& xi, const Atom atoms[], const unsigned& size);
 
+/* f(xi) prime is 1 if the distance from xi to center of ONLY one of these atoms
+is same than its radius */
+int calculate_fx_prime(const Point& xi, const Atom atoms[], const unsigned& size);
+
+
 /* calculate the total volume */
 void monte_carlo(const double& V, const unsigned int& n,
                                       const Box& box, const Atom atoms [],
                                       const unsigned& size);
+/* calculate the toal surface area */
+void monte_carlo_like(const double& SA, const unsigned int& N,
+                      const Atom atoms [],
+                      const unsigned& M);
 
 int main(int argc, const char * argv[]) {
    
@@ -129,21 +145,34 @@ int main(int argc, const char * argv[]) {
    int size; // the number of rows of data in the input file
    std::ifstream fin;
    open_file(size, fin);
-   
    Atom atoms[size];
-   
+   unsigned int N = 12900;
+
    process_file(fin, box, atoms);
    
    fin.close();
    
    double V = box.volume();
-   unsigned int N = 12900;
+      
+   double SA = get_total_surface_area(atoms, size);
    
-   //   for (int i = 0 ; i < size; i++) {
-   //      cout << atoms[i].get_x() << "\t" << atoms[i].get_y() << "\t" << atoms[i].get_z() << endl;
-   //   }
+   std::cout << SA << std::endl;
    
-   monte_carlo(V,N, box, atoms, size);
+   monte_carlo(V, N, box, atoms, size);
+
+//   monte_carlo_like(SA, N, atoms, size);
+}
+
+double get_total_surface_area(Atom atoms [], const unsigned& N)
+{
+   double SA = 0;
+   double pi = 3.14159265358979323846;
+   for (int i = 0; i < N; i++)
+   {
+      double curr_sa = 4*pi*atoms[i].get_r();
+      SA += curr_sa;
+   }
+   return SA;
 }
 
 void open_file(int& size, std::ifstream& fin)
@@ -206,6 +235,21 @@ Point get_random_point(const Box& box)
    return Point(random_x, random_y, random_z);
 }
 
+Point get_random_point_prime(const Atom& atom)
+{
+   double x_min = atom.get_x() - atom.get_r();
+   double x_max = atom.get_x() + atom.get_r();
+   double y_min = atom.get_y() - atom.get_r();
+   double y_max = atom.get_y() + atom.get_r();
+   double random_x = get_random_double(x_min, x_max);
+   double random_y = get_random_double(y_min, y_max);
+   double x_square = (random_x - atom.get_x()) * (random_x - atom.get_x());
+   double y_square = (random_y - atom.get_y()) * (random_y - atom.get_y());
+   double r_square = (atom.get_r()) * (atom.get_r());
+   double random_z = sqrt(r_square - x_square - y_square);
+   return Point(random_x, random_y, random_z);
+}
+
 double get_distance(const Point& p, const Atom& a)
 {
    double d_x = p.get_x() - a.get_x();
@@ -226,6 +270,20 @@ int calculate_fx(const Point& xi, const Atom atoms[], const unsigned& size)
    return 0;
 }
 
+int calculate_fx_prime(const Point& xi, const Atom atoms[], const unsigned& size)
+{
+   int counter = 0;
+   for(int i = 0; i < size; i++)
+   {
+      double distance = get_distance(xi, atoms[i]);
+      if(distance == atoms[i].get_r())
+         return 1;
+      if(counter == 2)
+         return 0;
+   }
+   return counter;
+}
+
 void monte_carlo(const double& V, const unsigned int& n,
                                       const Box& box, const Atom atoms [],
                                       const unsigned& size)
@@ -236,7 +294,6 @@ void monte_carlo(const double& V, const unsigned int& n,
    unsigned int N = n;
    
    double f = 0, f2 = 0, vol = 0, sd = 0;
-   auto results = std::make_pair(vol, sd);
    
    // create an ouput file
    std::ofstream fout(output_file);
@@ -279,3 +336,67 @@ void monte_carlo(const double& V, const unsigned int& n,
          << i  << "\n";
    }
 }
+
+void monte_carlo_like(const double& SA, const unsigned int& N,
+                                        const Atom atoms [],
+                                        const unsigned& M)
+{
+   double s = 0, s2 = 0;
+      
+   double f = 0, f2 = 0, sa = 0, sd = 0;
+   
+   // create an ouput file
+//   std::ofstream fout(output_file);
+   
+   // get the very begining timepoint
+   auto start = std::chrono::high_resolution_clock::now();
+   auto duration = std::chrono::duration_cast<std::chrono::microseconds>(start - start);
+
+   
+   // get N randoms points from the surface of all balls
+   std::vector<Point> points;
+   for(int i = 1 ; i < N; i++)
+   {
+      Point xi = get_random_point_prime(atoms[i%M]); // position xi at random at the surface
+      points.push_back(xi);
+   }
+
+   // find probability that a random point falls on the surface
+   // of exactly ONE of these balls
+   for(int i = 1 ; i < N; i++)
+   {
+      auto begin = std::chrono::high_resolution_clock::now();
+      
+      Point xi = points[i];
+      
+   
+      int f_of_xi = calculate_fx_prime(xi, atoms, M); // compute f(xi),
+      
+      // update the sums
+      s = s + f_of_xi;
+      s2 = s2 + f_of_xi*f_of_xi;
+      
+      // compute the means
+      f = s/i;
+      f2 = s2/i;
+      
+      sa = SA * f; // compute the volume
+      
+      // compute the standard deviation error
+      sd = sa * sqrt((f2-f*f)/i);
+            
+      // get stopping timepoint
+      auto stop = std::chrono::high_resolution_clock::now();
+      duration += std::chrono::duration_cast<std::chrono::microseconds>(stop - begin);
+      
+      // add results to the outputfile
+      std::cout << std::fixed << std::setprecision(2) << std::setw(10) << std::left
+         << sa << "\t"  << std::setw(10) /* estimated V*/
+         << sd <<"\t" << std::setw(10) /* estimated V lower bound*/
+         << sa - sd << "\t" << std::setw(10)
+         << sa + sd << "\t" << std::setw(10)
+         << duration.count() << "\t" << std::setw(10)
+         << i  << "\n";
+   }
+}
+
